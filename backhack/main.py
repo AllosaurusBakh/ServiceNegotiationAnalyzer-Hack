53211a90cf982e45840c00f7bd2f5dc4fccd9d4b
Alex_Kulich <allosaurusss@ya.ru>
2024-05-24 14:22:29 +0300

    Hot fix

--- backhack/main.py
+++ backhack/main.py
@@ -7,8 +7,10 @@ from typing import List
 
 app = FastAPI()
 origins = [
+    'http://127.0.0.1:3000',
     'http://127.0.0.1:5173',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'http://localhost:3000'
 ]
 app.add_middleware(
     CORSMiddleware,
