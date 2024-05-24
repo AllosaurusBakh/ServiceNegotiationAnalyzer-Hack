742226ff5c7698280149e8713822cf4402691430
Alex_Kulich <allosaurusss@ya.ru>
2024-05-24 12:15:13 +0300

    Minor update

--- backhack/main.py
+++ backhack/main.py
@@ -8,9 +8,7 @@ from typing import List
 app = FastAPI()
 origins = [
     'http://127.0.0.1:5173',
-    'http://127.0.0.1:3000',
-    'http://localhost:5173',
-    'http://localhost:3000'
+    'http://localhost:5173'
 ]
 app.add_middleware(
     CORSMiddleware,
